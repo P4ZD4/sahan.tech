e69668aa61b632054e97a7501f8b9495dae8a186
Sahan Ratnayake <sahanrat@gmail.com>
2024-10-03 14:32:20 -0400

    adds theming

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,16 @@
-import { useState } from 'react'
-import 'src/App.css'
+import '@/App.css'
+import { useTheme } from '@/components/theme-provider'
+import { Toggle } from '@/components/ui/toggle'
+import { SunMoon } from 'lucide-react'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const { toggleTheme } = useTheme()
 
   return (
     <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+      <Toggle className="absolute top-0 right-0 m-4 p-0 h-8 w-8" onClick={() => toggleTheme()}>
+        <SunMoon color="var(--foreground)" />
+      </Toggle>
     </>
   )
 }
---
+++ src/components/theme-provider.tsx
@@ -0,0 +1,71 @@
+import { createContext, useContext, useEffect, useState } from "react"
+
+type ThemeProviderProps = {
+  children: React.ReactNode
+  isDefaultDarkMode?: boolean
+  storageKey?: string
+}
+
+type ThemeProviderState = {
+  isDarkMode: boolean | undefined
+  toggleTheme: () => void
+}
+
+const initialState: ThemeProviderState = {
+  isDarkMode: true,
+  toggleTheme: () => {console.log('HERE')},
+}
+
+const ThemeProviderContext = createContext<ThemeProviderState>(initialState)
+
+export function ThemeProvider({
+  children,
+  isDefaultDarkMode = undefined,
+  storageKey = "vite-ui-theme",
+  ...props
+}: ThemeProviderProps) {
+  const [isDarkMode, setIsDarkMode] = useState<boolean | undefined>(
+    () => (localStorage.getItem(storageKey) === 'true') || isDefaultDarkMode
+  )
+
+  useEffect(() => {
+    const root = window.document.documentElement
+
+    root.classList.remove("light", "dark")
+
+    if (isDarkMode === undefined) {
+      const systemTheme = window.matchMedia("(prefers-color-scheme: dark)")
+        .matches
+        ? "dark"
+        : "light"
+
+      root.classList.add(systemTheme)
+      return
+    }
+
+    root.classList.add(isDarkMode ? 'dark' : 'light')
+  }, [isDarkMode])
+
+  const value = {
+    isDarkMode,
+    toggleTheme: () => {        
+        localStorage.setItem(storageKey, (!isDarkMode).toString())
+        setIsDarkMode(mode => !mode)
+    },
+  }
+
+  return (
+    <ThemeProviderContext.Provider {...props} value={value}>
+      {children}
+    </ThemeProviderContext.Provider>
+  )
+}
+
+export const useTheme = () => {
+  const context = useContext(ThemeProviderContext)
+
+  if (context === undefined)
+    throw new Error("useTheme must be used within a ThemeProvider")
+
+  return context
+}
