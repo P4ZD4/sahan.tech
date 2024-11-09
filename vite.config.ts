9d0c83a585cb1d047ad53a0d5654674cdb193699
Sahan Ratnayake <sahanrat@gmail.com>
2024-11-09 05:19:13 -0500

    adds ga4 tracking

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { VitePluginRadar } from 'vite-plugin-radar'
 import path from "path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePluginRadar({
+      analytics: {
+        id: 'G-34WVHE0Z1D',
+      },
+    })
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
